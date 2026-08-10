b2a7bc636a9d3bed8948c6476c2aa05d209b5834
Gaur-star <gaurav4star@gmail.com>
2026-08-10 17:56:15 +0530

    arrasy

--- main.java
+++ main.java
@@ -1,5 +1,26 @@
-public class main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+// public class main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello World");
+//   }
+// }
+
+
+public class main{
+  // public static void main(String[] args){
+
+  //   String[] cars = { "c1", "c2", "c3", "c4" };
+  
+  //   System.out.println(cars[1]);
+
+  // }
+
+
+  public static void main(String[] args){
+    int[] numbers = {1,2,3,4,5,6};
+    System.out.println(numbers[0]);
   }
+
+
+
+
 }

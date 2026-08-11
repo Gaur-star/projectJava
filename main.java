915f3943dd5478e89428f64d65976240009d2fa8
Gaur-star <gaurav4star@gmail.com>
2026-08-11 15:17:15 +0530

    pattern formation

--- main.java
+++ main.java
@@ -15,11 +15,44 @@ public class main{
   // }
 
 
-  public static void main(String[] args){
-    int[] numbers = {1,2,3,4,5,6};
-    System.out.println(numbers[0]);
+  // public static void main(String[] args){
+  //   int[] numbers = {1,2,3,4,5,6};
+  //   System.out.println(numbers[0]);
+  // }
+
+  // public static void main(String[] args){
+  //     int s = 10;
+  //     for(int i = 0; i < s; i++){
+  //       for(int j = 0; j <= i; j++){
+  //         System.out.println("*");
+  //       }
+  //     }
+  // }
+
+  public static void mymethod(){
+      int rows = 5;
+      for(int i = 1; i < rows; i++){
+        for(int j = 1; j <= i; j++){
+          System.out.print("*");
+        }
+        System.out.println();
+      }
   }
 
+  public static void main(String[] args){
+      mymethod();
+  }
+
+
+//     public static void main(String[] args) {
+//         int rows = 5;
+//         for (int i = 1; i <= rows; i++) { // Outer loop for rows
+//             for (int j = 1; j <= i; j++) { // Inner loop for stars
+//                 System.out.print("*");
+//             }
+//             System.out.println(); // Moves to the next line
+//         }
+//     }
 
 
 

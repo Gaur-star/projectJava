79fbdc805262f1298268f2b91a68933da903d3c6
Gaur-star <gaurav4star@gmail.com>
2026-08-19 18:50:51 +0530

    use of this

--- student_detail.java
+++ student_detail.java
@@ -2,13 +2,23 @@ class student_detail{
     String name;
     int class_no;
 
-    student_detail(){
-        name = "Anuj";
-        class_no = 9;
+    // student_detail(){
+    //     name = "Anuj";
+    //     class_no = 9;
+    // }
+    // public static void main(String[] args){
+    //     System.out.println("hello");
+    //     student_detail x = new student_detail();
+    //     System.out.println("your name is " + " " + x.name + " and you study in class " + x.class_no);
+    // }
+
+    student_detail(String name, int class_no){
+        this.name = name;
+        this.class_no = class_no;
     }
+
     public static void main(String[] args){
-        System.out.println("hello");
-        student_detail x = new student_detail();
+        student_detail x = new student_detail("Anuj", 9);
         System.out.println("your name is " + " " + x.name + " and you study in class " + x.class_no);
     }
 }

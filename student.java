45005da7757e91b57a1a36a0674a70fec7d56886
Gaur-star <gaurav4star@gmail.com>
2026-08-19 17:18:38 +0530

    constructor

---
+++ student.java
@@ -0,0 +1,12 @@
+class student{
+    String name;
+
+    student(){
+        name = "Rakesh";
+    }
+    public static void main(String[] args){
+        System.out.println("hello");
+        student x = new student();
+        System.out.println(x.name);
+    }
+}

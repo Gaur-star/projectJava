01fa0337aba457ab03a034614ab18c1b30196aea
Gaur-star <gaurav4star@gmail.com>
2026-08-22 18:13:44 +0530

    userdatainsert

--- User.java
+++ User.java
@@ -1,17 +1,49 @@
+// import java.util.Scanner;
+
+// class User{
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter any number");
+//         int age = sc.nextInt();
+        
+//         if(age > 12){
+//         System.out.println("User age is " + age + " .So User is teenager");
+
+//         }else 
+//         System.out.println("Adult");        
+//         }
+
+// }
+
+
 import java.util.Scanner;
 
 class User{
-
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter any number");
-        int age = sc.nextInt();
-        
-        if(age > 12){
-        System.out.println("User age is " + age + " .So User is teenager");
-
-        }else 
-        System.out.println("Adult");        
-        }
-
+        scanner sc = new Scanner(System.in);
+        System.out.println("hello number");
+        int age =  sc.nextLine();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
